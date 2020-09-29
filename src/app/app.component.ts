d5424a71c48030e263ac4ac97e07974fa8c08e5f
unknown <jonatan.garcia.schaff@gmail.com>
2020-09-29 00:29:55 -0300

    explanation directives and events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'capacitacion-angular';
+  author = 'Joni';
+  clickCounter = 1;
+  isHover = 'no';
+  participants = ['Joni', 'Manucho', 'Juan', 'Elias'];
+
+  clickme() {
+    this.clickCounter++;
+  }
+
+  hoverIn() {
+    this.isHover = 'yes';
+  }
+  
+  hoverOut() {
+    this.isHover = 'no';
+  }
+
 }
